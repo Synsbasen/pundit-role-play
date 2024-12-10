c6f6e4f9327e54536c4dbee59c5bcd545bd47bf4
Jimmy Poulsen <jimmypoulsen96@gmail.com>
2024-12-10 23:12:23 +0100

    Add model methods. Eg. User -> has_pundit_role

--- lib/pundit_role_play.rb
+++ lib/pundit_role_play.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
 require_relative "pundit_role_play/version"
+require_relative "pundit_role_play/role"
+require_relative "pundit_role_play/models"
 
 module PunditRolePlay
   class Error < StandardError; end
-  # Your code goes here...
+  class RoleNotFoundError < Error
+    def message
+      "Role does not exist"
+    end
+  end
+
+  if defined?(Rails)
+    ActiveSupport.on_load(:active_record) do
+      extend PunditRolePlay::Models
+    end
+  end
 end
+
+# TODO
+# - [ ] Define the roles somewhere - perhaps in a config file
+# - [x] Add model helpers to get role and check permissions
+#   - [x] Make sure that an error is raised if the role does not exist
+#   - [x] Make sure that permissions does not raise an error if the role is blank
+# - [ ] Add Pundit integration (like in BI)
---
+++ lib/pundit_role_play/models.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module PunditRolePlay
+  module Models
+    def has_pundit_role
+      include PunditRolePlay::ModelMethods
+    end
+  end
+
+  module ModelMethods
+    def role
+      value = super
+
+      return nil unless value.present?
+
+      value.constantize
+
+      value
+    rescue NameError
+      # Handle the case where the string can't be converted to a constant
+      # eg. "testrole" (which should be "TestRole")
+      #
+      # Handle the case where the constant does not exist
+      raise RoleNotFoundError
+    end
+
+    def pretty_role_name
+      role.gsub(/Role/, "")
+    end
+
+    def permissions
+      role.constantize.permissions
+    rescue NoMethodError
+      Permissions.new(nil)
+    end
+  end
+end
