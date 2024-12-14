64617c35bbee68f19d083c5f046e48049903d5d9
Jimmy Poulsen <jimmypoulsen96@gmail.com>
2024-12-14 17:46:35 +0100

    Remove spec

--- spec/pundit_role_play_spec.rb
+++ spec/pundit_role_play_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe PunditRolePlay do
   it "has a version number" do
     expect(PunditRolePlay::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
