b236dd18023efca52e861e623cf63cb305b408ae
Jimmy Poulsen <jimmypoulsen96@gmail.com>
2025-01-20 18:59:10 +0100

    Nil-safe user permissions

--- lib/pundit_role_play/adapter/pundit.rb
+++ lib/pundit_role_play/adapter/pundit.rb
@@ -4,11 +4,11 @@ module PunditRolePlay
   module Adapter
     module Pundit
       def can_read?(resource: self.class.to_s.gsub("Policy", "").pluralize.underscore)
-        user.permissions.can?(:read, resource.to_sym)
+        user&.permissions&.can?(:read, resource.to_sym) || false
       end
 
       def can_write?(resource: self.class.to_s.gsub("Policy", "").pluralize.underscore)
-        user.permissions.can?(:write, resource.to_sym)
+        user&.permissions&.can?(:write, resource.to_sym) || false
       end
     end
   end
